62b3b230c04778dd39219c61be6bcccc34c51ca0
Oliver <softwaredevdad@gmail.com>
2025-07-28 18:28:05 +0200

    modify auth controller to use async await

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -9,7 +9,7 @@ dotenv.config();
 const SALT_ROUNDS = 12;
 
 //#region Signup
-export function signup(req, res, next) {
+export async function signup(req, res, next) {
   const errors = validationResult(req);
   const { email, name, password } = req.body;
 
@@ -20,86 +20,69 @@ export function signup(req, res, next) {
       errors.array(),
     );
   }
-  bcrypt
-    .hash(password, SALT_ROUNDS)
-    .then((hashedPassword) => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        name: name,
-      });
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({ message: "User Created", userId: result._id });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
+  try {
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      name: name,
     });
+    const result = await user.save();
+    res.status(201).json({ message: "User Created", userId: result._id });
+  } catch (err) {
+    next(err);
+  }
 }
 //#endregion
 
 //#region Login
-export function login(req, res, next) {
+export async function login(req, res, next) {
   const { email, password } = req.body;
-  let loadedUser;
-
-  User.findOne({ email: email })
-    .then((user) => {
-      if (!user) {
-        // 401 -> Not Authenticated
-        throw new ApiError(401, "A user with this email could not be found");
-      }
-      loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((passwordMatch) => {
-      if (!passwordMatch) {
-        throw new ApiError(401, "Wrong password");
-      }
-      const token = jwt.sign(
-        { email: loadedUser.email, userId: loadedUser._id.toString() },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "1h",
-        },
-      );
-      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
-    })
-    .catch((err) => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      }
-      next(err);
-    });
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      // 401 -> Not Authenticated
+      throw new ApiError(401, "A user with this email could not be found");
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new ApiError(401, "Wrong password");
+    }
+    const token = jwt.sign(
+      { email: loadedUser.email, userId: loadedUser._id.toString() },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+    );
+    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  } catch (err) {
+    next(err);
+  }
 }
 //#endregion
 
 //#region Get Status
-export function getStatus(req, res, next) {
-  User.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        throw new ApiError(404, "User not found");
-      }
+export async function getStatus(req, res, next) {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
 
-      res
-        .status(200)
-        .json({ message: "Status Fetched Successfully", status: user.status });
-    })
-    .catch((err) => {
-      console.log(err);
-      console.log(err.statusCode);
-      next(err);
-    });
+    res
+      .status(200)
+      .json({ message: "Status Fetched Successfully", status: user.status });
+  } catch (err) {
+    console.log(err);
+    console.log(err.statusCode);
+    next(err);
+  }
 }
 //#endregion
 
 //#region Update Status
-export function updateStatus(req, res, next) {
+export async function updateStatus(req, res, next) {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -110,24 +93,20 @@ export function updateStatus(req, res, next) {
     );
   }
   const { status } = req.body;
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      throw new ApiError(404, "User not found");
+    }
 
-  User.findById(req.userId)
-    .then((user) => {
-      if (!user) {
-        throw new ApiError(404, "User not found");
-      }
-
-      user.status = status;
-      return user.save();
-    })
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({ message: "Status Updated" });
-    })
-    .catch((err) => {
-      console.log(err);
-      console.log(err.statusCode);
-      next(err);
-    });
+    user.status = status;
+    const result = await user.save();
+    console.log(result);
+    res.status(200).json({ message: "Status Updated" });
+  } catch (err) {
+    console.log(err);
+    console.log(err.statusCode);
+    next(err);
+  }
 }
 //#endregion
