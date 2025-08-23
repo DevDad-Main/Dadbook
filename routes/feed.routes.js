b32e560595cb42918b8d89df82ea76f368d30593
Oliver <softwaredevdad@gmail.com>
2025-08-23 19:55:16 +0200

    add feed routes back from github before merge of graphql

---
+++ routes/feed.routes.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/feed.controllers.js";
+import {
+  createPostValidation,
+  createPutValidation,
+} from "../utils/validation.utils.js";
+import { isAuthenticated } from "../middleware/isAuthenticated.middleware.js";
+
+const router = express.Router();
+
+//#region Get Routes
+router.get("/posts", isAuthenticated, getPosts);
+router.get("/post/:postId", isAuthenticated, getPost);
+//#endregion
+
+//#region Post Routes
+router.post("/post", isAuthenticated, createPostValidation, createPost);
+//#endregion
+
+//#region Put Routes
+router.put("/post/:postId", isAuthenticated, createPutValidation, updatePost);
+//#endregion
+
+//#region Delete Routes
+router.delete("/post/:postId", isAuthenticated, deletePost);
+//#endregion
+
+export default router;
