14a56e81eaf598132f51bceb0a9c91c5f345ba2c
Oliver <softwaredevdad@gmail.com>
2025-08-23 20:10:05 +0200

    refactor app.js to use cors with custom config and organise code

--- app.js
+++ app.js
@@ -6,59 +6,60 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-import multer from "multer";
 import { Server } from "socket.io";
 import { config } from "./socket.js";
+import cors from "cors";
 
+//#region CONSTANTS
 dotenv.config();
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+//#endregion;
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/jpg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
+//#region CORS
 // This is what we used previously x-www-form-urlencoded when using forms
 // app.use(bodyParser.urlencoded({ extended: true }));
 // But we are working with json now -> application/json
-app.use(bodyParser.json());
-app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"),
-);
-app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE",
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+const allowedOrigins = process.env.CORS_ORIGIN.split(","); // split comma-separated string
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["PATCH", "POST", "PUT", "GET", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Access-Control-Allow-Headers",
+      // "Access-Control-Allow-Origin",
+    ],
+    credentials: true,
+    // optionsSuccessStatus: 200,
+  }),
+);
+//#endregion
+
+//#region Middleware
+app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/feed", feedRoutes);
 app.use("/auth", authRoutes);
+//#endregion
 
+//#region MongoDB/Mongoose Connection w/Socket IO
 mongoose
   .connect(process.env.MONGODB_URI)
   .then((result) => {
@@ -73,3 +74,4 @@ mongoose
     });
   })
   .catch((err) => console.log(err));
+//#endregion
